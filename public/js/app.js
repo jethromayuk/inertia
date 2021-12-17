e13e23e33bab8a0b86472281e38fca38436d7409
Jethro May <jethro.may@minttwist.com>
2021-12-17 09:55:37 +0200

    Added base login methods

--- public/js/app.js
+++ public/js/app.js
@@ -526,6 +526,14 @@ __webpack_require__.r(__webpack_exports__);
 /***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
 var map = {
+	"./Auth/Login": [
+		"./resources/js/Pages/Auth/Login.vue",
+		"resources_js_Pages_Auth_Login_vue"
+	],
+	"./Auth/Login.vue": [
+		"./resources/js/Pages/Auth/Login.vue",
+		"resources_js_Pages_Auth_Login_vue"
+	],
 	"./Home": [
 		"./resources/js/Pages/Home.vue",
 		"resources_js_Pages_Home_vue"
--- routes/web.php
+++ routes/web.php
@@ -5,59 +5,53 @@ use Inertia\Inertia;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Request;
+use App\Http\Controllers\Auth\LoginController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/login', [LoginController::class, 'create'])->name('login');
 
-Route::get('/', function () {
-    return Inertia::render('Home');
-});
+Route::middleware('auth')->group(function() {
+    Route::get('/', function () {
+        return Inertia::render('Home');
+    });
 
-Route::get('/users', function () {
-    return Inertia::render('Users/Index', [
-        'users' => User::query()
-            ->when(Request::input('search'), function($query, $search) {
-                $query->where('name', 'like', "%{$search}%");
-            })
-            ->paginate(10)
-            ->withQueryString()
-            ->through(fn($user) => [
-            'id' => $user->id,
-            'name' => $user->name
-            ]),
-        'filters' => Request::only(['search'])
-    ]);
-});
+    Route::get('/users', function () {
+        return Inertia::render('Users/Index', [
+            'users' => User::query()
+                ->when(Request::input('search'), function($query, $search) {
+                    $query->where('name', 'like', "%{$search}%");
+                })
+                ->paginate(10)
+                ->withQueryString()
+                ->through(fn($user) => [
+                'id' => $user->id,
+                'name' => $user->name
+                ]),
+            'filters' => Request::only(['search'])
+        ]);
+    });
 
-Route::get('/users/create', function() {
-    return Inertia::render('Users/Create');
-});
+    Route::get('/users/create', function() {
+        return Inertia::render('Users/Create');
+    });
 
-Route::post('/users', function() {
-    // Validate
-    $user = Request::validate([
-        'name' => 'required',
-        'email' => ['required', 'email'],
-        'password' => 'required',
-    ]);
-    // Create user
-    User::create($user);
-    // Redirect
-    return redirect('/users');
-});
+    Route::post('/users', function() {
+        // Validate
+        $user = Request::validate([
+            'name' => 'required',
+            'email' => ['required', 'email'],
+            'password' => 'required',
+        ]);
+        // Create user
+        User::create($user);
+        // Redirect
+        return redirect('/users');
+    });
 
-Route::get('/settings', function () {
-    return Inertia::render('Settings');
-});
+    Route::get('/settings', function () {
+        return Inertia::render('Settings');
+    });
 
-Route::post('/logout', function () {
-    dd('Logging out');
+    Route::post('/logout', function () {
+        dd('Logging out');
+    });
 });
